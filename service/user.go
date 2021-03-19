3aa76e6683de0d3f3370fc7ee378ccee9d6ecede
Nicks-code-space <rcravistein@yahoo.in>
2021-03-18 22:16:54 -0400

    add structured logging with zerolog

--- service/user.go
+++ service/user.go
@@ -3,3 +3,6 @@ package main
 // user.go
 log.Info().Str("method", r.Method).Msg("request received")
 metrics.RequestCount.WithLabelValues(route).Inc()
+if err != nil {
+	return nil, fmt.Errorf("db query failed: %w", err)
+}
