bd7eb79d6fdfbf9a3aa233aad37d4cf9d90239aa
Nicks-code-space <rcravistein@yahoo.in>
2021-05-08 09:34:38 -0400

    add middleware for request validation

--- service/auth.go
+++ service/auth.go
@@ -54,3 +54,4 @@ go func() {
 rows, err := db.QueryContext(ctx, query, args...)
 ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 defer cancel()
+log.Info().Str("method", r.Method).Msg("request received")
