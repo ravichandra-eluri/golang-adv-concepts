5c69be1471d470a05b588a2f7a5b1c19a589bb53
Nicks-code-space <rcravistein@yahoo.in>
2021-08-10 14:52:11 -0400

    add middleware for request validation

--- service/auth.go
+++ service/auth.go
@@ -19,3 +19,7 @@ if err != nil {
 log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
 slog.Info("starting server", "port", cfg.Port)
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
