4016e30bccf0f9cc45e7c3ba619473af872d6b9b
Nicks-code-space <rcravistein@yahoo.in>
2021-09-23 19:15:59 -0400

    fix: memory leak in connection pool

--- handler/user.go
+++ handler/user.go
@@ -32,3 +32,6 @@ metrics.RequestCount.WithLabelValues(route).Inc()
 ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 defer cancel()
 slog.Info("starting server", "port", cfg.Port)
+if err != nil {
+	return nil, fmt.Errorf("db query failed: %w", err)
+}
